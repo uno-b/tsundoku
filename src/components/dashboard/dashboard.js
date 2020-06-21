444fb59c0d4e42aa609069f714b28cabd9971af6
Uno <bg.unuu@yahoo.com>
2020-06-21 22:47:27 +0300

    commit

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,84 +1,92 @@
 import React from "react";
+import flashcardPic from "../../img/flashcards.png";
+import challengesPic from "../../img/challenges.png";
+import rewardsPic from "../../img/rewards.png";
+import statisticsPic from "../../img/statistics.png";
 
 const Dashboard = () => {
   return (
-    <div class="row">
-      <div class="col s12 l6">
-        <div class="card">
-          <div class="card-image">
-            <img src="../../img/flashcards.jpeg" alt="Flashcard" />
-          </div>
-          <div class="card-content">
-            <span class="card-title">Flashcards</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-              sed, delestrum soluta! Repellendus sequi quas quam. Quod odio qui
-              ipsam!
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="#">Daily Challenge</a>
-            <a href="#">Study</a>
-            <a href="#">Manage Flashcards</a>
+    <div className="container">
+      <div class="row">
+        <div class="col s12 l6">
+          <div class="card">
+            <div class="card-image">
+              <img src={flashcardPic} alt="Flashcard" />
+              <a href="#" class="halfway-fab btn-floating orange">
+                <i class="material-icons">notes</i>
+              </a>
+            </div>
+
+            <div class="card-content">
+              <span class="card-title">Flashcards</span>
+              <p>Review the flashcards you have created.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Daily Challenge</a>
+              <span> | </span>
+              <a href="#">Manage</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col s12 l6">
-        <div class="card">
-          <div class="card-image">
-            <img src="../../img/challenges.png" alt="Challenges" />
-          </div>
-          <div class="card-content">
-            <span class="card-title">Challenges</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-              sed, delectus, oluptates nostrum soluta! Repellendus sequi quas
-              quam. Quod odio qui ipsam!
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="#">Add a Challenge</a>
-            <a href="#">View Challenges</a>
+        <div class="col s12 l6">
+          <div class="card">
+            <div class="card-image">
+              <img src={challengesPic} alt="Challenges" />
+              <a href="#" class="halfway-fab btn-floating orange">
+                <i class="material-icons">notes</i>
+              </a>
+            </div>
+
+            <div class="card-content">
+              <span class="card-title">Challenges</span>
+              <p>Complete challenges to earn points.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Add a Challenge</a>
+              <span> | </span>
+              <a href="#">View</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col s12 l6">
-        <div class="card">
-          <div class="card-image">
-            <img src="../../img/rewards.png" alt="Rewards" />
-          </div>
-          <div class="card-content">
-            <span class="card-title">Rewards</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-              sed, delectus,s voluptates nostrum soluta! Repellendus sequi quas
-              quam. Quod odio qui ipsam!
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="#">Add a Reward</a>
-            <a href="#">View Rewards</a>
+        <div class="col s12 l6">
+          <div class="card">
+            <div class="card-image">
+              <img src={rewardsPic} alt="Rewards" />
+              <a href="#" class="halfway-fab btn-floating orange">
+                <i class="material-icons">notes</i>
+              </a>
+            </div>
+
+            <div class="card-content">
+              <span class="card-title">Rewards</span>
+              <p>Earn your rewards with your points.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Add a Reward</a>
+              <span> | </span>
+              <a href="#">View</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col s12 l6">
-        <div class="card">
-          <div class="card-image">
-            <img src="../../img/statistics.png" alt="Statistics" />
-          </div>
-          <div class="card-content">
-            <span class="card-title">Statistics</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-              sed, delectus, et voluptates nostrum soluta! Repellendus sequi
-              quas quam. Quod odio qui ipsam!
-            </p>
-          </div>
-          <div class="card-action">
-            <a href="#">View Statistics</a>
+        <div class="col s12 l6">
+          <div class="card">
+            <div class="card-image">
+              <img src={statisticsPic} alt="Statistics" />
+              <a href="#" class="halfway-fab btn-floating orange">
+                <i class="material-icons">notes</i>
+              </a>
+            </div>
+
+            <div class="card-content">
+              <span class="card-title">Statistics</span>
+              <p>See your current progress.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">View</a>
+            </div>
           </div>
         </div>
       </div>
